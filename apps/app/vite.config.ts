decdcbc251ccc0c0525105ed7be0c941153a8a84
EdwardKerckhof <edward@zonedev.be>
2021-12-14 02:20:52 +0100

    Add unplugin-icons

--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -1,4 +1,5 @@
 import vue from '@vitejs/plugin-vue'
+import icons from 'unplugin-icons/vite'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
@@ -6,5 +7,5 @@ export default defineConfig({
   server: {
     port: 5000,
   },
-  plugins: [vue()],
+  plugins: [vue(), icons()],
 })
