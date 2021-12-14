27f5e8885e59c00db70f87f4ec014163dc81b99e
EdwardKerckhof <edward@zonedev.be>
2021-12-14 02:47:51 +0100

    Add vite-plugin-pages

--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -1,11 +1,18 @@
 import vue from '@vitejs/plugin-vue'
 import icons from 'unplugin-icons/vite'
 import { defineConfig } from 'vite'
+import pages from 'vite-plugin-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 5000,
   },
-  plugins: [vue(), icons()],
+  plugins: [
+    vue(),
+    // https://github.com/antfu/unplugin-icons (icon set from Icônes)
+    icons(),
+    // https://github.com/hannoeru/vite-plugin-pages (file system based routing)
+    pages()
+  ],
 })
