0b2f7723f579a7d58bac4a05297851906bc1aabe
EdwardKerckhof <edward@zonedev.be>
2021-12-17 19:54:50 +0100

    Remove app controller

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common'
 
-import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { PostsModule } from './posts/posts.module'
 import { PrismaModule } from './prisma/prisma.module'
@@ -8,7 +7,6 @@ import { UsersModule } from './users/users.module'
 
 @Module({
   imports: [UsersModule, PostsModule, PrismaModule],
-  controllers: [AppController],
   providers: [AppService]
 })
 export class AppModule {}
